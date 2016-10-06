e9eed5d15be2562cc2c599b0e96ff1db5c2dbf3e
EGOIST <0x142857@gmail.com>
2016-10-06 17:32:51 +0800

    handle error

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 'use strict'
 
 module.exports = (input, char) => {
+  if (!input || typeof input !== 'string') {
+    throw new Error('Expected input to be a string')
+  }
   char = char || ''
   const index = input.indexOf(char)
-  return [
+  return index === -1 ? null : [
     input.substr(0, index),
     input.substr(index + char.length)
   ]
